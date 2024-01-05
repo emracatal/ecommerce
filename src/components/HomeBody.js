b00b45350acd1678056e641a65e56c21729c2309
emracatal <134175723+emracatal@users.noreply.github.com>
2024-01-05 23:17:02 +0300

    bestseller9 to homebody

--- src/components/HomeBody.js
+++ src/components/HomeBody.js
@@ -1,25 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import Productcard from "./Productcard";
+import Productcard from "../trash/Productcard";
 import Carousel from "./Carousel";
 import Carousel2 from "./Carousel2";
 import containerfluid from "../assets/containerfluid.png";
 
 import Blogcard from "./Blogcard";
-import Productcard2 from "./Productcard2";
+import axios from "axios";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import axiosInstance from "../api/api";
+import axiosInstanceLocal from "../api/apiLocal";
 
 export default function HomeBody() {
-  const products = [
-    { id: 1, name: "product1", price: 19.99 },
-    { id: 2, name: "product2", price: 19.99 },
-    { id: 3, name: "product3", price: 39.99 },
-    { id: 4, name: "product4", price: 49.99 },
-    { id: 5, name: "product5", price: 19.99 },
-    { id: 6, name: "product6", price: 19.99 },
-    { id: 7, name: "product7", price: 19.99 },
-    { id: 8, name: "product8", price: 19.99 },
-  ];
+  const [products, setProducts] = useState([]);
 
+  useEffect(() => {
+    const fetchBestSellerProducts = async () => {
+      try {
+        const response = await axiosInstanceLocal.get("/products/bestseller8");
+        if (response.status === 200) {
+          setProducts(response.data);
+        } else {
+          throw new Error("Failed to fetch products");
+        }
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchBestSellerProducts();
+  }, []);
   const blogs = [
     { id: 1, name: "blog1", price: 19.99 },
     { id: 2, name: "blog2", price: 19.99 },
@@ -70,8 +80,52 @@ export default function HomeBody() {
         </div>
         <div className="bestseller-products-container flex items-center justify-center max-w-[1440px]">
           <div className="flex flex-row flex-wrap max-w-[1050px] gap-7 justify-center items-canter">
-            {products.map((product, i) => (
-              <Productcard key={i} product={product} />
+            {products?.map((product, i) => (
+              <Link
+                key={i}
+                to={`/product/${product.category_id}/${
+                  product.id
+                }/${product.name.toLowerCase().replaceAll(" ", "-")}`}
+              >
+                <div className="card max-w-[239px] min-h-[526px] shadow-lg hover:bg-verylightgray2 hover:scale-105">
+                  <img
+                    src={product.image}
+                    className="shadow-sm"
+                    alt={product.name}
+                  />
+                  <div className="cardbody flex flex-auto flex-col items-center gap-2 py-6">
+                    <h5 className="font-bold">{product.name}</h5>
+                    <p className="text-center line-clamp-2">
+                      {product.description}
+                    </p>
+                    <div className="price flex flex-row gap-3">
+                      <h5 className="text-lightgray">
+                        {[...Array(5)].map((star, index) => {
+                          const ratingValue = index + 1;
+                          return (
+                            <span key={index}>
+                              {product.rating >= ratingValue ? (
+                                <i className="fa-solid fa-star text-yellow-500"></i>
+                              ) : (
+                                <i className="fa-regular fa-star text-yellow-500"></i>
+                              )}
+                            </span>
+                          );
+                        })}
+                      </h5>
+                      <h5 className="text-secondarycolor1 font-bold">
+                        {product.price} TL
+                      </h5>
+                    </div>
+                    <div className="colors flex gap-1 ">
+                      <i className="fa-solid fa-circle text-turku"></i>
+                      <i className="fa-solid fa-circle text-secondarycolor1"></i>
+                      <i className="fa-solid fa-circle text-alertcolor"></i>
+                      <i className="fa-solid fa-circle text-darkblue"></i>
+                    </div>
+                  </div>
+                </div>
+              </Link>
             ))}
           </div>
         </div>
