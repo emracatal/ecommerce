d5c3b739aba43107eafa98ba70ef3a4d927ec8fe
emracatal <134175723+emracatal@users.noreply.github.com>
2024-01-21 17:15:58 +0300

    checkout address initiated

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -5,8 +5,8 @@ export default function Checkout() {
   return (
     <>
       <HeaderHome />
-      <div className="flex justify-center bg-verylightgray3 ">
-        <div className="container flex justify-between items-center max-w-[1050px] min-h-[48px] mobile:flex mobile:flex-col mobile:py-6 mobile:gap-7 ">
+      <div className="flex justify-center bg-darborder-darkblue3 ">
+        <div className="container flex justify-between items-center max-w-[1050px] min-h-[48px] mobile:flex mobile:flex-col mobile:py-1 mobile:gap-7 ">
           <div className="text-darkblue font-bold flex gap-2 ">
             <h3>Checkout</h3>
           </div>
@@ -18,7 +18,7 @@ export default function Checkout() {
         <div className="container flex items-start max-w-[1050px] gap-5">
           <div className="flex flex-col justify-center  w-[50%] gap-1">
             <div>
-              <h1 class="font-semibold text-xl py-2">
+              <h1 class="font-semibold text-xl py-1">
                 1-Select delivery address
               </h1>
             </div>
@@ -26,29 +26,52 @@ export default function Checkout() {
               onClick={() =>
                 document.getElementById("myDIV").classList.toggle("hidden")
               }
-              className="border border-gray-400 text-gray-600 shadow-lg p-3 rounded-md cursor-pointer text-center"
+              className="border border-gray-400 text-gray-600 shadow-lg p-2 rounded-md cursor-pointer text-center"
             >
               <h3> + </h3>
               <h5> Add a new address </h5>
             </button>
 
-            <div id="myDIV" className="hidden" class="container mx-auto pt-10">
+            <div
+              id="myDIV"
+              className="hidden"
+              class="container mx-auto pt-1 border-2 border-turku rounded-lg"
+            >
               <div class="mx-auto max-w-xl">
-                <h1 class="font-bold text-gray-900 not-italic text-2xl text-center">
-                  A Tailwind CSS Address Grid Form
-                </h1>
-
-                <form class="bg-white shadow rounded px-8 pt-6 pb-8 mb-4 mt-4">
-                  <div class="flex flex-wrap -mx-3 mb-6">
-                    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <form class="bg-white shadow rounded">
+                  {/* address title */}
+                  <div class="flex flex-wrap -mx-3 mb-1">
+                    <div class="w-full px-3">
                       <label
-                        class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                        class="block tracking-wide text-gray-800 text-sm font-bold mb-1"
+                        for="grid-address-1"
+                      >
+                        Address Title
+                      </label>
+                      <input
+                        class="appearance-none block w-full bg-g ray-200 text-gray-700 border border-darkblue rounded py-1 px-4 mb-1 leading-tight focus:outline-none focus:bg-white"
+                        id="grid-address-1"
+                        type="text"
+                        placeholder="1234 Main Street"
+                      />
+                      <p class=" text-red-500 text-xs italic">
+                        Please enter an address title
+                      </p>
+                    </div>
+                  </div>
+
+                  {/* firstname lastname */}
+
+                  <div class="flex flex-wrap -mx-3 mb-1">
+                    <div class="w-full md:w-1/2 px-3 mb-1 md:mb-1">
+                      <label
+                        class="block  tracking-wide text-gray-700 text-xs font-bold mb-1"
                         for="grid-first-name"
                       >
                         First Name
                       </label>
                       <input
-                        class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                        class="appearance-none block w-full bg-gray-200 text-gray-700 border border-darkblue rounded py-1 px-4 mb-1 leading-tight focus:outline-none focus:bg-white"
                         id="grid-first-name"
                         type="text"
                         placeholder="Jane"
@@ -59,68 +82,69 @@ export default function Checkout() {
                     </div>
                     <div class="w-full md:w-1/2 px-3">
                       <label
-                        class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                        class="block  tracking-wide text-gray-700 text-xs font-bold mb-1"
                         for="grid-last-name"
                       >
                         Last Name
                       </label>
                       <input
-                        class="appearance-none block w-full bg-g ray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                        class="appearance-none block w-full bg-g ray-200 text-gray-700 border border-gray-200 rounded py-1 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                         id="grid-last-name"
                         type="text"
                         placeholder="Doe"
                       />
                     </div>
                   </div>
-
-                  <div class="flex flex-wrap -mx-3 mb-6">
+                  {/* phone */}
+                  <div class="flex flex-wrap -mx-3 mb-1">
                     <div class="w-full px-3">
                       <label
-                        class="block tracking-wide text-gray-800 text-sm font-bold mb-2"
-                        for="grid-address-1"
+                        class="block tracking-wide text-gray-800 text-sm font-bold mb-1"
+                        for="grid-phone"
                       >
-                        Address Line 1
+                        Phone
                       </label>
                       <input
-                        class="appearance-none block w-full bg-g ray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                        class="appearance-none block w-full bg-g ray-200 text-gray-700 border border-darkblue rounded py-1 px-4 mb-1 leading-tight focus:outline-none focus:bg-white"
+                        id="grid-phone"
+                        type="tel"
+                        placeholder="+1-123-456-7890"
+                      />
+                    </div>
+                  </div>
+                  {/* address */}
+
+                  <div class="flex flex-wrap -mx-3 mb-1">
+                    <div class="w-full px-3">
+                      <label
+                        class="block tracking-wide text-gray-800 text-sm font-bold mb-1"
+                        for="grid-address-1"
+                      >
+                        Address
+                      </label>
+                      <input
+                        class="appearance-none block w-full bg-g ray-200 text-gray-700 border border-darkblue rounded py-1 px-4 mb-1 leading-tight focus:outline-none focus:bg-white"
                         id="grid-address-1"
                         type="text"
                         placeholder="1234 Main Street"
                       />
-                      <p class="text-xs italic">
+                      <p class="text-red-500 text-xs italic">
                         Please enter your street address.
                       </p>
                     </div>
                   </div>
 
-                  <div class="flex flex-wrap -mx-3 mb-6">
-                    <div class="w-full px-3">
+                  <div class="flex flex-wrap -mx-3 mb-1">
+                    <div class="w-full md:w-1/3 px-3 mb-1 md:mb-1">
                       <label
-                        class="block tracking-wide text-gray-800 text-sm font-bold mb-2"
-                        for="grid-address-2"
-                      >
-                        Address Line 2
-                      </label>
-                      <input
-                        class="appearance-none block w-full bg-g ray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                        id="grid-address-2"
-                        type="text"
-                        placeholder="Suite ABC"
-                      />
-                    </div>
-                  </div>
-
-                  <div class="flex flex-wrap -mx-3 mb-2">
-                    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                      <label
-                        class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                        class="block  tracking-wide text-gray-700 text-xs font-bold mb-1"
                         for="grid-city"
                       >
                         City
                       </label>
                       <div class="relative">
                         <input
-                          class="appearance-none block w-full bg-g ray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                          class="appearance-none block w-full bg-g ray-200 text-gray-700 border border-gray-200 rounded py-1 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                           id="grid-city"
                           type="text"
                           placeholder="Los Angeles"
@@ -136,17 +160,17 @@ export default function Checkout() {
                         </div>
                       </div>
                     </div>
-                    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                    <div class="w-full md:w-1/3 px-3 mb-1 md:mb-1">
                       <label
-                        class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                        for="grid-state"
+                        class="block  tracking-wide text-gray-700 text-xs font-bold mb-1"
+                        for="grid-district"
                       >
-                        State
+                        District
                       </label>
                       <div class="relative">
                         <select
-                          class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                          id="grid-state"
+                          class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-1 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                          id="grid-district"
                         >
                           <option>California</option>
                         </select>
@@ -161,54 +185,28 @@ export default function Checkout() {
                         </div>
                       </div>
                     </div>
-                    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                    <div class="w-full md:w-1/3 px-3 mb-1 md:mb-1">
                       <label
-                        class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                        for="grid-zip"
+                        class="block  tracking-wide text-gray-700 text-xs font-bold mb-1"
+                        for="grid-neighborhood"
                       >
-                        Zip
+                        Neighborhood
                       </label>
                       <input
-                        class="appearance-none block w-full bg-g ray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                        id="grid-zip"
+                        class="appearance-none block w-full bg-g ray-200 text-gray-700 border border-gray-200 rounded py-1 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                        id="grid-neighborhood"
                         type="text"
                         placeholder="90020"
                       />
                     </div>
                   </div>
 
-                  <div class="flex flex-wrap -mx-3 mb-6">
-                    <div class="w-full px-3">
-                      <label
-                        class="block tracking-wide text-gray-800 text-sm font-bold mb-2"
-                        for="grid-phone"
-                      >
-                        Phone
-                      </label>
-                      <input
-                        class="appearance-none block w-full bg-g ray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                        id="grid-phone"
-                        type="tel"
-                        placeholder="+1-123-456-7890"
-                      />
-                    </div>
-                  </div>
-
-                  <div class="mb-6">
-                    <label class="flex items-center text-gray-800 text-sm font-bold">
-                      <input class="mr-2 leading-tight" type="checkbox" />
-                      <span class="text-sm">
-                        Receive order status updates via text message
-                      </span>
-                    </label>
-                  </div>
-
-                  <div class="flex justify-between items-center mb-6">
+                  <div class="flex w-full justify-center ">
                     <button
-                      class="bg-indigo-700 h-12 hover:shadow hover:bg-indigo-500 text-white font-bold px-4 rounded focus:outline-none focus:shadow-outline"
+                      class="bg-turku h-10 w-full  text-white font-bold px-4 rounded focus:outline-none focus:shadow-outline"
                       type="button"
                     >
-                      Next
+                      Submit
                     </button>
                   </div>
                 </form>
@@ -225,7 +223,7 @@ export default function Checkout() {
               />
               <label
                 for="bordered-radio-1"
-                class="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                class="w-full py-1 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
               >
                 Default radio
               </label>
@@ -241,9 +239,9 @@ export default function Checkout() {
               />
               <label
                 for="bordered-radio-2"
-                class="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                class="w-full py-1 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
               >
-                Checked state
+                Checked district
               </label>
             </div>
           </div>
@@ -251,7 +249,7 @@ export default function Checkout() {
           {/* payment */}
           <div className="flex flex-col justify-center  w-[50%] gap-1 ">
             <div>
-              <h1 class="font-semibold text-xl py-2">
+              <h1 class="font-semibold text-xl py-1">
                 2-Select payment details
               </h1>
             </div>
@@ -265,7 +263,7 @@ export default function Checkout() {
               />
               <label
                 for="bordered-radio-1"
-                class="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                class="w-full py-1 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
               >
                 Default radio
               </label>
@@ -281,9 +279,9 @@ export default function Checkout() {
               />
               <label
                 for="bordered-radio-2"
-                class="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                class="w-full py-1 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
               >
-                Checked state
+                Checked district
               </label>
             </div>
           </div>
