aa79ae55de56720daf6aae9c2f50dbf7758f85d5
emracatal <134175723+emracatal@users.noreply.github.com>
2024-02-10 20:58:18 +0300

    done

--- src/components/OrdersList.js
+++ src/components/OrdersList.js
@@ -28,7 +28,7 @@ export default function OrdersList() {
       <div className="flex justify-center bg-verylightgray3 ">
         <div className="container  flex justify-between items-center max-w-[1050px] min-h-[92px] pr-2 mobile:flex mobile:flex-col mobile:py-2 mobile:gap-7 ">
           <div className="text-darkblue font-bold flex gap-2">
-            <h3>Order List</h3>
+            <h3>Order Listxx</h3>
           </div>
         </div>
       </div>
